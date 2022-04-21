ae50147d1c916fb9dd184efcae3b58b4103e14d5
Robson Rosa <contatorobsonrosa@gmail.com>
2022-04-21 15:13:12 -0300

    pwa init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StylesProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
 import GlobalStyle from "./commons/styles/global-style";
+import Main from './containers/Main'
 
 function App() {
   return (
@@ -9,9 +10,7 @@ function App() {
       <StylesProvider injectFirst>
         <CssBaseline/>
         <GlobalStyle/>
-        <div>
-          true
-          </div>
+        <Main/>
       </StylesProvider>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import * as serviceWorker from './serviceWorker'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,4 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-
+serviceWorker.register()
